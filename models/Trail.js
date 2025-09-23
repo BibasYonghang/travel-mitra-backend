d4753df1dbb0b4a6eb53b49e9c8d5a41cf0b09d4
BibasYonghang <bibasyonghang100@gmail.com>
2025-09-23 09:54:53 +0545

    insert data in the database through seed

--- models/Trail.js
+++ models/Trail.js
@@ -4,11 +4,16 @@ const trailSchema = new mongoose.Schema({
   name: { type: String, required: true },
   location: { type: String, required: true },
   difficulty: { type: String, enum: ["Easy", "Moderate", "Hard"], required: true },
-  distance: { type: Number, required: true }, // in km
-  duration: { type: String }, // e.g., "5 hours"
+  distance: { type: Number, required: true },
+  duration: { type: String },
   description: { type: String },
-  image: { type: String } // URL to trail image
+  image: { type: String },
+  coordinates: {
+    lat: { type: Number },
+    lng: { type: Number }
+  }
 }, { timestamps: true });
+
 
 const Trail = mongoose.model("Trail", trailSchema);
 export default Trail;
--- seed/seedTrails.js
+++ seed/seedTrails.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import Trail from "./models/Trail.js";
+import Trail from "../models/Trail.js";
 
 dotenv.config();
 
